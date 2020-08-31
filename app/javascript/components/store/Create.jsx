c3a238464d859d60e81939498337f16af7b7f265
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-08-31 17:18:42 +0530

    made the basic react setup and created the form view to create store

--- app/javascript/components/store/Create.jsx
+++ app/javascript/components/store/Create.jsx
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import API from "../../utils/API";
 
-const Create = () => {
-  return (
-    <div>
-      <p>create</p>
-    </div>
-  );
-};
+class Create extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      store: {
+        name: "",
+      },
+    };
+  }
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ store: { ...this.state.store, [name]: value } });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let { store } = this.state;
+    API.postNewTask("/stores", "POST", store)
+      .then((response) => {
+        window.location.href = "/";
+      })
+      .catch((error) => {
+        error.json().then(({ errors }) => {
+          console.log(errors);
+        });
+      });
+  };
+
+  displayForm = () => {
+    let { store } = this.state;
+    return (
+      <div>
+        <p>create Store</p>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            value={store.name}
+            placeholder="Enter store name"
+            onChange={this.handleChange}
+          />
+          <input type="submit" value="Create Store" />
+        </form>
+      </div>
+    );
+  };
+
+  render() {
+    return <>{this.displayForm()}</>;
+  }
+}
 
 export default Create;
---
+++ app/javascript/utils/Routes.js
@@ -0,0 +1,7 @@
+export function stores_path() {
+  return "/stores";
+}
+
+export function new_store_path() {
+  return "/stores/new";
+}
