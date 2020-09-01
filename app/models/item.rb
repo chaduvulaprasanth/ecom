21c73fc08d2c5f8aad15fdfeda401e85a451fcfa
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-09-01 12:24:15 +0530

    written tests for item name

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  belongs_to :store
+  validates :name, presence: :true
 end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @store = stores(:prasanth)
+    @item = @store.items.build(name: "apple", cost: 20)
+  end
+
+  test "item should be valid" do
+    assert @item.valid?
+  end
+
+ test "name should not be empty" do
+   @item.name = ""
+   assert_not @item.valid?
+ end
 end
